895af330669c9cef15883d36617310b260bf71cf
audree fletcher <audreefletcher@audrees-MacBook-Pro.local>
2019-10-11 17:35:39 +0100

    End of Friday session

--- app/routes.js
+++ app/routes.js
@@ -13,7 +13,7 @@ const utils = require('../lib/utils.js')
 
 // Page routes
 const userNeeds = require('./data/user-needs.json')
-
+const services = require('./data/servicelist.json')
 // Add your routes here - above the module.exports line
 
 
@@ -21,39 +21,44 @@ router.get('/needs', function (req, res) {
   res.render('needs', userNeeds);
 })
 
-
-router.get('/services', function (req, res) {
-
-  // Look through a folder, get all of the files, store it as the files variable
-  fs.readdir(path.join(__dirname, '/data/collections/'), (err, files) => {
-    // create an empty array to store files
-    let collections = [];
-    // Loop over each 'file' in 'files'
-    files.forEach((file) => {
-      // parse each file using gray-matter
-      let fileContent = matter.read(__dirname + '/data/collections/' + file.toString());
-      collections.push(fileContent);
-    });
-    // render the page, pass in all the files as data
-    res.render('services', { 'data': collections });
-  })
+router.get('/servicelist', function (req, res) {
+  res.render('servicelist', services);
 })
 
-router.get('/services/:page', function (req, res) {
-  // If the link already has .md on the end (for GitHub docs)
-  // remove this when we render the page
-  if (req.params.page.slice(-3).toLowerCase() === '.md') {
-    req.params.page = req.params.page.slice(0, -3)
-  }
-  // get the relevant file
-  let fileLocation = path.join(__dirname, '/data/collections/' + req.params.page + '.md')
-  // use matter to parse the data from the file
-  var article = matter.read(fileLocation);
-  // convert markdown in matter to html
-  article.content = marked(article.content)
-  // render the page, pass in the data
-  res.render('services-template', { 'document': article })
-})
+
+//
+// router.get('/services', function (req, res) {
+//
+//   // Look through a folder, get all of the files, store it as the files variable
+//   fs.readdir(path.join(__dirname, '/data/collections/'), (err, files) => {
+//     // create an empty array to store files
+//     let collections = [];
+//     // Loop over each 'file' in 'files'
+//     files.forEach((file) => {
+//       // parse each file using gray-matter
+//       let fileContent = matter.read(__dirname + '/data/collections/' + file.toString());
+//       collections.push(fileContent);
+//     });
+//     // render the page, pass in all the files as data
+//     res.render('services', { 'data': collections });
+//   })
+// })
+//
+// router.get('/services/:page', function (req, res) {
+//   // If the link already has .md on the end (for GitHub docs)
+//   // remove this when we render the page
+//   if (req.params.page.slice(-3).toLowerCase() === '.md') {
+//     req.params.page = req.params.page.slice(0, -3)
+//   }
+//   // get the relevant file
+//   let fileLocation = path.join(__dirname, '/data/collections/' + req.params.page + '.md')
+//   // use matter to parse the data from the file
+//   var article = matter.read(fileLocation);
+//   // convert markdown in matter to html
+//   article.content = marked(article.content)
+//   // render the page, pass in the data
+//   res.render('services-template', { 'document': article })
+// })
 
 module.exports = router
 
